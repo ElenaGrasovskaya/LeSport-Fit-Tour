3ba968505ad53f52e9b0385a0de9fa35011694d6
ElenaGrasovskaya <elenagrasovskaya@gmail.com>
2021-12-23 14:01:31 +0200

    Changes in tour description

--- sender.php
+++ sender.php
@@ -1,8 +1,9 @@
 <?php
     $name = $_POST['name'];
-   
+    $surname = $_POST['surname'];
+	$phone = $_POST['phone'];
     $email = $_POST['email'];
-
+    $text = $_POST['text'];
 
 	$to = "elenagrasovskaya@gmail.com"; 
 	$date = date ("d.m.Y"); 
@@ -13,10 +14,13 @@
 	
 	$msg="
     Имя: $name /n
- 
+    Фамилия: $surname /n
+    Телефон: $phone /n
     Почта: $email /n
-	mail($to, $subject, "From: $from ");
+    Текст: $text"; 	
+	mail($to, $subject, $msg, "From: $from ");
 
 ?>
 
-<p>Привет, форма отправлена</p>
+<p style="font-size: 3rem; text-align: center; margin: 10rem;">Спасибо за заявку!</p>
+<a href="index.html" style="font-size: 2rem;  margin: 3rem auto; text-decoration:none; color: #45858C">Назад &larr;</a>
